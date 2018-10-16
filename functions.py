ef173b0bec4f540e990b55a3551254071febf8fa
patykov <admin@admins-MacBook-Air.local>
2018-10-15 23:21:27 -0300

    Adding backtraking line search

--- functions.py
+++ functions.py
@@ -3,12 +3,12 @@ import numpy as np
 
 def dichotomos_search(f, xl, xu, uncertainty_range, max_iter=20):
     num_iter = 0
-    eps = uncertainty_range/10
+    eps = uncertainty_range / 10
 
     while (xu - xl > uncertainty_range):
-        x = (xu + xl)/2
-        xa = x - eps/2
-        xb = x + eps/2
+        x = (xu + xl) / 2
+        xa = x - eps / 2
+        xb = x + eps / 2
 
         num_iter += 1
         if f(xa) < f(xb):
@@ -24,21 +24,21 @@ def dichotomos_search(f, xl, xu, uncertainty_range, max_iter=20):
 
 def fibonacci_search(f, xl, xu, uncertainty_range):
     Il = (xu - xl)
-    Fn = Il/(uncertainty_range)
+    Fn = Il / (uncertainty_range)
 
     F = [1, 1]
     while F[-1] < Fn:
-        F.append(sum(F[-2:])*1.0)
+        F.append(sum(F[-2:]) * 1.0)
     n = len(F)
 
-    Il *= (F[n-2]/F[n-1])
+    Il *= (F[n - 2] / F[n - 1])
     xa = xu - Il
     xb = xl + Il
     fa = f(xa)
     fb = f(xb)
 
-    for k in range(1, n-1):
-        Il *= (F[n-k-2]/F[n-k-1])
+    for k in range(1, n - 1):
+        Il *= (F[n - k - 2] / F[n - k - 1])
 
         if fa >= fb:
             xl = xa
@@ -63,7 +63,7 @@ def golden_section_search(f, xl, xu, uncertainty_range, max_iter=20):
     Il = (xu - xl)
     K = 1.618034
 
-    Il *= 1/K
+    Il *= 1 / K
     xa = xu - Il
     xb = xl + Il
     fa = f(xa)
@@ -71,7 +71,7 @@ def golden_section_search(f, xl, xu, uncertainty_range, max_iter=20):
 
     k = 1
     while (Il >= uncertainty_range) and (xa <= xb):
-        Il *= 1/K
+        Il *= 1 / K
         k += 1
 
         if fa >= fb:
@@ -88,11 +88,11 @@ def golden_section_search(f, xl, xu, uncertainty_range, max_iter=20):
             fa = f(xa)
 
     if fa > fb:
-        x = 0.5*(xb + xu)
+        x = 0.5 * (xb + xu)
     elif fa == fb:
-        x = 0.5*(xa + xb)
+        x = 0.5 * (xa + xb)
     elif fa < fb:
-        x = 0.5*(xl + xa)
+        x = 0.5 * (xl + xa)
 
     return x, f(x), k
 
@@ -100,17 +100,17 @@ def golden_section_search(f, xl, xu, uncertainty_range, max_iter=20):
 def quadratic_interpolation_search(f, x1, x3, uncertainty_range, max_iter=50):
     x0 = 10**99
 
-    x2 = 0.5*(x1 + x3)
+    x2 = 0.5 * (x1 + x3)
     f1 = f(x1)
     f2 = f(x2)
     f3 = f(x3)
 
     for k in range(1, max_iter):
-        a = (x2 - x3)*f1
-        b = (x3 - x1)*f2
-        c = (x1 - x2)*f3
+        a = (x2 - x3) * f1
+        b = (x3 - x1) * f2
+        c = (x1 - x2) * f3
 
-        x = (a*(x2 + x3) + b*(x3 + x1) + c*(x1 + x2))/(2*(a + b + c))
+        x = (a * (x2 + x3) + b * (x3 + x1) + c * (x1 + x2)) / (2 * (a + b + c))
         fx = f(x)
         if abs(x0 - x) < uncertainty_range:
             break
@@ -147,24 +147,24 @@ def cubic_interpolation_search(f, f_, x1, x2, x3, uncertainty_range, max_iter=50
     f3 = f(x3)
 
     for k in range(1, max_iter):
-        beta = (f2 - f1 + f_1*(x1 - x2)) / ((x1 - x2)**2)
-        gama = (f3 - f1 + f_1*(x1 - x3)) / ((x1 - x3)**2)
-        theta = (2*(x1**2) - x2*(x1 + x2)) / (x1 - x2)
-        psi = (2*(x1**2) - x3*(x1 + x3)) / (x1 - x3)
+        beta = (f2 - f1 + f_1 * (x1 - x2)) / ((x1 - x2)**2)
+        gama = (f3 - f1 + f_1 * (x1 - x3)) / ((x1 - x3)**2)
+        theta = (2 * (x1**2) - x2 * (x1 + x2)) / (x1 - x2)
+        psi = (2 * (x1**2) - x3 * (x1 + x3)) / (x1 - x3)
 
         a3 = (beta - gama) / (theta - psi)
-        a2 = beta - theta*a3
-        a1 = f_1 - 2*a2*x1 - 3*a3*(x1**2)
+        a2 = beta - theta * a3
+        a1 = f_1 - 2 * a2 * x1 - 3 * a3 * (x1**2)
 
-        if (a2**2 - 3*a1*a3) < 0:
+        if (a2**2 - 3 * a1 * a3) < 0:
             raise Exception(
                 'Negative square root! Initializate the algorithm with different points.')
-        x_ext_1 = (-a2 - (a2**2 - 3*a1*a3)**(0.5)) / (3*a3)
-        x_ext_2 = (-a2 + (a2**2 - 3*a1*a3)**(0.5)) / (3*a3)
+        x_ext_1 = (-a2 - (a2**2 - 3 * a1 * a3)**(0.5)) / (3 * a3)
+        x_ext_2 = (-a2 + (a2**2 - 3 * a1 * a3)**(0.5)) / (3 * a3)
 
-        if 2*a2 + 6*a3*x_ext_1 > 0:
+        if 2 * a2 + 6 * a3 * x_ext_1 > 0:
             x = x_ext_1
-        elif 2*a2 + 6*a3*x_ext_2 > 0:
+        elif 2 * a2 + 6 * a3 * x_ext_2 > 0:
             x = x_ext_2
 
         fx = f(x)
@@ -194,7 +194,7 @@ def davies_swann_campey(f, x0, uncertainty_range):
 
     k = 0
     while (1):
-        k +=1
+        k += 1
         # Step 2
         jump = False
         k += 1
@@ -211,11 +211,11 @@ def davies_swann_campey(f, x0, uncertainty_range):
             f_1 = f(x_1)
             if f_1 < f0:
                 p = -1
-            else: 
-                #f_1 >= f0 <= f1 
+            else:
+                # f_1 >= f0 <= f1
                 # Go to step 7
                 jump = True
-        
+
         if not jump:
             # Step 4
             last_f = f0
@@ -226,14 +226,14 @@ def davies_swann_campey(f, x0, uncertainty_range):
                 new_f = f(last_x + (exp2 * p * delta))
                 if new_f > last_f:
                     break
-                
+
                 last_2f = last_f
                 last_f = new_f
-                last_x +=  exp2 * p * delta
+                last_x += exp2 * p * delta
                 exp2 *= 2
 
             # Step 5
-            xm = last_x + (exp2/2 * p * delta)
+            xm = last_x + (exp2 / 2 * p * delta)
             fm = f(xm)
 
             # Step 6
@@ -241,15 +241,17 @@ def davies_swann_campey(f, x0, uncertainty_range):
                 if last_2f - fm == 0:
                     x0 = x_1
                 else:
-                    x0 = x_1 + (exp2/4)*(p * delta * (last_2f - fm))/(last_2f - 2*last_f + fm)
+                    x0 = x_1 + (exp2 / 4) * (p * delta *
+                                             (last_2f - fm)) / (last_2f - 2 * last_f + fm)
 
             else:
                 if last_f - new_f == 0:
                     x0 = xm
                 else:
-                    x0 = xm + (exp2/4)*(p * delta * (last_f - new_f))/(last_f - 2*fm + new_f)
-                    
-            if exp2/2 * delta < uncertainty_range:
+                    x0 = xm + (exp2 / 4) * (p * delta *
+                                            (last_f - new_f)) / (last_f - 2 * fm + new_f)
+
+            if exp2 / 2 * delta < uncertainty_range:
                 return x0, f(x0), k
             else:
                 delta *= K
@@ -258,9 +260,28 @@ def davies_swann_campey(f, x0, uncertainty_range):
 
         # Step 7
         if f_1 - f1 != 0:
-            x0 += (delta * (f_1 - f1))/(2*(f_1 - 2*f0 + f1))
+            x0 += (delta * (f_1 - f1)) / (2 * (f_1 - 2 * f0 + f1))
 
         if delta < uncertainty_range:
             return x0, f(x0), k
         else:
             delta *= K
+
+
+def backtraking_line_search(f, f_, x, uncertainty_range):
+    alfa = 0.2
+    beta = 0.1
+
+    t = 1
+    k = 1
+    while (1):
+        delta_x = -1 * np.sign(f_(x))
+        while f(x + t * delta_x) > (f(x) + alfa * t * np.transpose(f_(x)) * delta_x):
+            t *= beta
+            k += 1
+
+        x_min = x + t * delta_x
+        if abs(x - x_min) < uncertainty_range:
+            return x_min, f(x_min), k
+        else:
+            x = x_min
--- lista01.py
+++ lista01.py
@@ -6,71 +6,79 @@ import functions
 
 def get_f(num):
     if num == 4.2:
+
         def f(x):
-            return -5*x**5 + 4*x**4 - 12*x**3 + 11*x**2 - 2*x + 1
+            return -5 * x**5 + 4 * x**4 - 12 * x**3 + 11 * x**2 - 2 * x + 1
 
     return f
 
 
 def get_f_(num):
     if num == 4.2:
+
         def f_(x):
-            return -25*x**4 + 16*x**3 - 36*x**2 + 22*x - 2
+            return -25 * x**4 + 16 * x**3 - 36 * x**2 + 22 * x - 2
+
     return f_
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        '-exe_num',
-        help='Number of the exercise.',
-        required=True, type=float)
+    parser.add_argument('-exe_num', help='Number of the exercise.', required=True, type=float)
     args = parser.parse_args()
 
     # Exercicio 4.2
     f = get_f(args.exe_num)
     f_ = get_f_(args.exe_num)
 
+    print('Exercise {}:\n'.format(args.exe_num))
     t = time.time()
     min_x, min_f_x, num_iter = functions.dichotomos_search(f, -0.5, 0.5, 10**(-5))
-    delta_t = (time.time() - t)*1000
-    print('Exercise 4.2: Dichotomous Search')
+    delta_t = (time.time() - t) * 1000
+    print('Dichotomous Search')
     print('x: {:.5f}, f(x): {:.5f}, num_iter: {}, time: {:.5f} ms\n'.format(
         min_x, min_f_x, num_iter, delta_t))
 
     t = time.time()
     min_x, min_f_x, num_iter = functions.fibonacci_search(f, -0.5, 0.5, 10**(-5))
-    delta_t = (time.time() - t)*1000
-    print('Exercise 4.2: Fibonacci Search')
+    delta_t = (time.time() - t) * 1000
+    print('Fibonacci Search')
     print('x: {:.5f}, f(x): {:.5f}, num_iter: {}, time: {:.5f} ms\n'.format(
         min_x, min_f_x, num_iter, delta_t))
 
     t = time.time()
     min_x, min_f_x, num_iter = functions.golden_section_search(f, -0.5, 0.5, 10**(-5))
-    delta_t = (time.time() - t)*1000
-    print('Exercise 4.2: Golden Section Search')
+    delta_t = (time.time() - t) * 1000
+    print('Golden Section Search')
     print('x: {:.5f}, f(x): {:.5f}, num_iter: {}, time: {:.5f} ms\n'.format(
         min_x, min_f_x, num_iter, delta_t))
 
     t = time.time()
     min_x, min_f_x, num_iter = functions.quadratic_interpolation_search(f, -0.5, 0.5, 10**(-5))
-    delta_t = (time.time() - t)*1000
-    print('Exercise 4.2: Quadratic Interpolation Search')
+    delta_t = (time.time() - t) * 1000
+    print('Quadratic Interpolation Search')
     print('x: {:.5f}, f(x): {:.5f}, num_iter: {}, time: {:.5f} ms\n'.format(
         min_x, min_f_x, num_iter, delta_t))
 
     t = time.time()
     # RUN WITH DIFFERENT POINTS X1, X2, X3
     min_x, min_f_x, num_iter = functions.cubic_interpolation_search(f, f_, -0.2, 0.1, 0.2, 10**(-5))
-    delta_t = (time.time() - t)*1000
-    print('Exercise 4.2: Cubic Interpolation Search')
+    delta_t = (time.time() - t) * 1000
+    print('Cubic Interpolation Search')
     print('x: {:.5f}, f(x): {:.5f}, num_iter: {}, time: {:.5f} ms\n'.format(
         min_x, min_f_x, num_iter, delta_t))
 
     t = time.time()
     # RUN WITH DIFFERENT POINTS X0
     min_x, min_f_x, num_iter = functions.davies_swann_campey(f, 0.2, 10**(-5))
-    delta_t = (time.time() - t)*1000
-    print('Exercise 4.2: Davies, Swann and Campey Search')
+    delta_t = (time.time() - t) * 1000
+    print('Davies, Swann and Campey Search')
+    print('x: {:.5f}, f(x): {:.5f}, num_iter: {}, time: {:.5f} ms\n'.format(
+        min_x, min_f_x, num_iter, delta_t))
+
+    t = time.time()
+    min_x, min_f_x, num_iter = functions.backtraking_line_search(f, f_, -0.5, 10**(-5))
+    delta_t = (time.time() - t) * 1000
+    print('Backtraking Line Search')
     print('x: {:.5f}, f(x): {:.5f}, num_iter: {}, time: {:.5f} ms\n'.format(
         min_x, min_f_x, num_iter, delta_t))
