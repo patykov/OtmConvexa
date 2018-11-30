9d9acd6d41eafb368d8eabfb49c462179a2dd4b6
patykov <admin@B88L07-17.redmond.corp.microsoft.com>
2018-11-30 00:23:52 -0200

    Adding functions, not working yet

--- lista04/functions.py
+++ lista04/functions.py
@@ -8,11 +8,11 @@ from lista02 import functions as f2
 
 def iterative_conjugate_gradient(f, g, H, p, eps):
     # initial point
-    x, fx, k = f3.conjugate_gradient(f, g, H, p, eps)
+    x, fx, k = f3.conjugate_gradient(f, g, H, [p, 0.1], eps)
     t = np.arange(0.2, 10000, 0.1)
     all_k = k
     for ti in t:
-        x, fx, k = f3.conjugate_gradient(f, g, H, x, eps, t=ti)
+        x, fx, k = f3.conjugate_gradient(f, g, H, [x, ti], eps)
         all_k += k
 
     return x, fx, all_k
--- lista04/lista04.py
+++ lista04/lista04.py
@@ -1,5 +1,6 @@
 import argparse
 import time
+import math
 import numpy as np
 from scipy.linalg import null_space
 import functions
@@ -8,29 +9,53 @@ import functions
 def get_defined_functions(exe_num):
     if exe_num == 11.16:
         A = np.array([[1, 2, 1, 2], [1, 1, 2, 4]])
-        b = np.array([[3], [5]])
         c = np.array([1, 1.5, 1, 1])
 
         F = null_space(A)
         x0 = [[1], [0], [0], [1]]
 
-        def f(z, t):
+        def f(x):
+            z = x[0]
+            t = x[1]
             f0 = np.dot((t * c), (np.dot(F, z) + x0))
-            I_ = [np.ln(np.dot(F[i, :], z) + x0[i]) for i in range(4)]
+            I_ = [math.log(np.dot(F[i, :], z) + x0[i]) for i in range(4)]
 
             # f = (t)*c'*(F*z + x0) -
-            # (log(F(1,:)*z + x0(1))) - (log(F(2,:)*z + x0(2))) - (log(F(3,:)*z + x0(3))) - (log(F(4,:)*z + x0(4)))
+            # (log(F(1,:)*z + x0(1))) - (log(F(2,:)*z + x0(2))) -
+            # (log(F(3,:)*z + x0(3))) - (log(F(4,:)*z + x0(4)))
 
             return f0 + sum(I_)
 
-        def g(z, t):
-            divs = [x0[i] + F[i,1]*z[1] + F[i,2]*z[2]]
-            g = [
-                F11*t*conj(c1) - F21/(x02 + F21*z1 + F22*z2) - F31/(x03 + F31*z1 + F32*z2) - F41/(x04 + F41*z1 + F42*z2) - F11/(x01 + F11*z1 + F12*z2) + F21*t*conj(c2) + F31*t*conj(c3) + F41*t*conj(c4);
-                F12*t*conj(c1) - F22/(x02 + F21*z1 + F22*z2) - F32/(x03 + F31*z1 + F32*z2) - F42/(x04 + F41*z1 + F42*z2) - F12/(x01 + F11*z1 + F12*z2) + F22*t*conj(c2) + F32*t*conj(c3) + F42*t*conj(c4)];
+        def g(x):
+            z = x[0]
+            t = x[1]
+            logs_divs = [(x0[i] + F[i, 0] * z[0] + F[i, 1] * z[1]) for i in range(4)]
+            logs_dev = np.array([[F[i, 0] / logs_divs[i] for i in range(4)],
+                                 [F[i, 1] / logs_divs[i] for i in range(4)]])
+            f0_dev = np.array([[F[i, 0] * t * np.conj(c[i]) for i in range(4)],
+                               [F[i, 1] * t * np.conj(c[i]) for i in range(4)]])
+
+            return sum(f0_dev) - sum(logs_dev)
+
+        def H(x):
+            z = x[0]
+            logs_divs = [(x0[i] + F[i, 0] * z[0] + F[i, 1] * z[1]) for i in range(4)]
+            logs_dev = np.array([[F[i, 0] / logs_divs[i] for i in range(4)],
+                                 [F[i, 1] / logs_divs[i] for i in range(4)]])
+
+            aux = logs_dev**2
+            dz1z1 = aux[0]
+            dz2z2 = aux[1]
+            dz1z2 = np.array([F[i, 0] * F[i, 1] for i in range(4)]) * 1.0 / aux
+
+            return np.array([[dz1z1, dz1z2], [dz1z2, dz2z2]])
+
+        return f, g, H
 
 
-        return f
+def get_points(exe_num):
+    if exe_num == 11.16:
+        return [2, -1]
 
 
 if __name__ == '__main__':
@@ -39,11 +64,11 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     eps = 10**(-6)
-    # points = get_points(args.exe_num)
+    points = get_points(args.exe_num)
 
     if args.exe_num == 11.16:
         f, g, H = get_defined_functions(args.exe_num)
         t = time.time()
-        x, fx, k = functions.iterative_conjugate_gradient(f, g, H, p, eps)
+        x, fx, k = functions.iterative_conjugate_gradient(f, g, H, points, eps)
         delta_t = (time.time() - t) * 1000
         print('x: {}, fx: {:.5f}, num_iter: {}, time: {:.5f} ms\n'.format(x, fx, k, delta_t))
