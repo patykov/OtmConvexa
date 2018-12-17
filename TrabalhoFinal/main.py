cb334f1b96dd3095cedfb1af8267b0000fceb68f
Patricia Kovaleski <v-pakova@microsoft.com>
2018-12-17 01:33:31 +0000

    Getting to an end.

---
+++ TrabalhoFinal/main.py
@@ -0,0 +1,76 @@
+from __future__ import division, print_function
+
+import glob
+import os
+import time
+
+import numpy as np
+from PIL import Image
+from skimage import filters
+
+from pcp import my_pcp
+
+
+def bitmap_to_mat(bitmap_seq):
+    """from blog.shriphani.com"""
+    matrix = []
+    shape = None
+    for bitmap_file in bitmap_seq:
+        img = Image.open(bitmap_file).convert("L")
+        if shape is None:
+            shape = img.size
+        assert img.size == shape
+        img = np.array(img.getdata())
+        matrix.append(img)
+    return np.array(matrix), shape[::-1]
+
+
+if __name__ == "__main__":
+    dataset = 2
+
+    if dataset == 1:
+        frames_dir = "C:/Users/v-pakova/source/repos/OtmConvexa/TrabalhoFinal/dataset/" \
+                    "MovedObject"
+        files_list = glob.glob("{}/*.bmp".format(frames_dir))
+        sorted_list = sorted(files_list, key=lambda x: int(
+            os.path.basename(x).replace('.bmp', '').replace('b', '')))[1380:1440]
+
+    elif dataset == 2:
+        frames_dir = "E:/TCC/Datasets/UCF-101_rgb/BodyWeightSquats/v_BodyWeightSquats_g01_c01.avi/"
+        files_list = glob.glob("{}/*.jpg".format(frames_dir))
+        sorted_list = sorted(files_list, key=lambda x: int(
+            os.path.basename(x).replace('.jpg', '')))[:60]
+
+    elif dataset == 3:
+        frames_dir = "C:/Users/v-pakova/source/repos/OtmConvexa/TrabalhoFinal/dataset/" \
+                    "capoeira"
+        files_list = glob.glob("{}/*.jpg".format(frames_dir))
+        sorted_list = sorted(files_list, key=lambda x: int(
+            os.path.basename(x).replace('.jpg', '')))[:60]
+
+    M, shape = bitmap_to_mat(sorted_list)
+    print(M.shape)
+
+    t = time.time()
+
+    L, S = my_pcp(M, shape, connected=True)
+
+    delta_t = (time.time() - t) * 1000
+    print("Total time: {:.5f} ms".format(delta_t))
+
+    threshold = filters.threshold_otsu(S)
+    St = S < threshold
+    St = np.array(St, dtype=np.float32)*255
+
+    output_dir = 'data{}-result-pcp-block-10-4'.format(dataset)
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
+
+    for i in range(min(len(M), 500)):
+        data = [M, L, S, St]
+        text = ['original', 'low_rank', 'sparse', 'sparse_t']
+        for d, t in zip(data, text):
+            img = Image.fromarray(d[i].reshape(shape))
+            if img.mode != 'RGB':
+                img = img.convert('RGB')
+            img.save("{}/{}_{}.png".format(output_dir, i, t))
--- TrabalhoFinal/pcp.py
+++ TrabalhoFinal/pcp.py
@@ -1,106 +1,88 @@
-"""
-https://github.com/dfm/pcp/blob/master/pcp.py
-An implementation of the Principal Component Pursuit algorithm for robust PCA
-as described in `Candes, Li, Ma, & Wright <http://arxiv.org/abs/0912.3599>`_.
-An alternative Python implementation using non-standard dependencies and
-different hyperparameter choices is available at:
-http://blog.shriphani.com/2013/12/18/
-    robust-principal-component-pursuit-background-matrix-recovery/
-"""
-
-from __future__ import division, print_function
-
-import logging
-import time
-
-import fbpca
 import numpy as np
-from scipy.sparse.linalg import svds
-
-__all__ = ["pcp"]
-
-
-def pcp(M, delta=1e-6, mu=None, maxiter=500, verbose=False, missing_data=True,
-        svd_method="approximate", **svd_args):
-    # Check the SVD method.
-    allowed_methods = ["approximate", "exact", "sparse"]
-    if svd_method not in allowed_methods:
-        raise ValueError("'svd_method' must be one of: {0}"
-                         .format(allowed_methods))
-
-    # Check for missing data.
-    shape = M.shape
-    if missing_data:
-        missing = ~(np.isfinite(M))
-        if np.any(missing):
-            M = np.array(M)
-            M[missing] = 0.0
-    else:
-        missing = np.zeros_like(M, dtype=bool)
-        if not np.all(np.isfinite(M)):
-            logging.warn("The matrix has non-finite entries. "
-                         "SVD will probably fail.")
-
-    # Initialize the tuning parameters.
-    lam = 1.0 / np.sqrt(np.max(shape))
-    if mu is None:
-        mu = 0.25 * np.prod(shape) / np.sum(np.abs(M))
-        if verbose:
-            print("mu = {0}".format(mu))
-
-    # Convergence criterion.
-    norm = np.sum(M ** 2)
-
-    # Iterate.
-    i = 0
-    rank = np.min(shape)
-    S = np.zeros(shape)
-    Y = np.zeros(shape)
-    while i < max(maxiter, 1):
-        # SVD step.
-        u, s, v = _svd(svd_method, M - S + Y / mu, rank+1, 1./mu, **svd_args)
-
-        s = shrink(s, 1./mu)
-        rank = np.sum(s > 0.0)
-        u, s, v = u[:, :rank], s[:rank], v[:rank, :]
-        L = np.dot(u, np.dot(np.diag(s), v))
-
-        # Shrinkage step.
-        S = shrink(M - L + Y / mu, lam / mu)
-
-        # Lagrange step.
-        step = M - L - S
-        step[missing] = 0.0
-        Y += mu * step
-
-        # Check for convergence.
-        err = np.sqrt(abs(np.sum(step ** 2) / norm))
-        if verbose:
-            print("Iteration {0}: error={1:.3e}, rank={2:d}, nnz={3:d}".format(
-                i, err, np.sum(s > 0), np.sum(S > 0)))
-        if err < delta:
-            break
-        i += 1
-
-    if i >= maxiter:
-        logging.warn("convergence not reached in pcp")
-    return L, S, (u, s, v)
 
 
 def shrink(M, tau):
     return np.sign(M) * np.maximum((np.abs(M) - tau), np.zeros(M.shape))
 
 
-def _svd(method, X, rank, tol, **args):
-    rank = min(rank, np.min(X.shape))
-    if method == "approximate":
-        return fbpca.pca(X, k=rank, raw=True, **args)
-    elif method == "exact":
-        return np.linalg.svd(X, full_matrices=False, **args)
-    elif method == "sparse":
-        if rank >= np.min(X.shape):
-            return np.linalg.svd(X, full_matrices=False)
-        u, s, v = svds(X, k=rank, tol=tol)
-        u, s, v = u[:, ::-1], s[::-1], v[::-1, :]
-        return u, s, v
-    raise ValueError("invalid SVD method")
+def my_pcp(M, f_shape, delta=1e-3, maxiter=50, connected=True, verbose=True):
+    shape = M.shape
+    m = np.max(shape)
+
+    # Initialize the tuning parameters.
+    lamb = 1.0 / np.sqrt(m)
+    neigh = 0.05*np.array(f_shape)
+    mu = 0.25 * np.prod(shape) / np.sum(np.abs(M))
+    print("mu = {0}".format(mu))
+    lamb2 = 10**(-4)
+
+    # Convergence criterion.
+    norm = np.sum(M ** 2)
+
+    # Iterate.
+    rank = np.min(shape)
+
+    S = np.zeros(shape)
+    Y = np.zeros(shape)
+    for k in range(maxiter):
+        # Update L
+        u, s, v = np.linalg.svd(M - S + Y / mu, full_matrices=False)
+        s = shrink(s, 1./mu)
+        rank = np.sum(s > 0.0)
+        u, s, v = u[:, :rank], s[:rank], v[:rank, :]
+        L = np.dot(u, np.dot(np.diag(s), v))
+
+        # Update S
+        if connected:
+            F = []
+            # Iterate over lines of s (frames)
+            for l, fl in enumerate(S):
+                f = np.reshape(fl, f_shape)
+                new_f = np.zeros(f_shape)
+
+                for i in range(f_shape[0]):
+                    x_min = max(0, int(i-neigh[0]/2))
+                    x_max = min(f_shape[0], int(i+neigh[0]/2))
+                    for j in range(f_shape[1]):
+                        y_min = max(0, int(j-neigh[1]/2))
+                        y_max = min(f_shape[1], int(j+neigh[1]/2))
+
+                        block = f[x_min:x_max, y_min:y_max]
+
+                        diff = f[i, j] - block
+
+                        norm2_block = np.linalg.norm(diff, ord=2)
+                        prod_fij = - lamb2 * diff/(norm2_block if norm2_block > 0 else 1)
+
+                        # new_f[x_min:x_max, y_min:y_max] += lamb2 * block/(
+                        #     norm2_block if norm2_block > 0 else 1)
+
+                        # w = np.exp(-(diff_m**2)/sigma)
+                        # prod_fij = w * lamb2 * np.sign(diff_f)
+
+                        new_f[x_min:x_max, y_min:y_max] += prod_fij
+                        new_f[i, j] -= sum(sum(prod_fij))
+
+                F.append(np.reshape(new_f, m))
+
+            S_ = M - L + Y/mu - F
+
+        else:
+            S_ = M - L + Y/mu
+
+        S = shrink(S_, lamb / mu)
+
+        # Update Y
+        step = M - L - S
+        Y += mu * step
+
+        # Check for convergence
+        err = np.sqrt(abs(np.sum(step ** 2) / norm))
+        if verbose:
+            print("Iteration {0}: error={1:.3e}, rank={2:d}, nnz={3:d}".format(
+                k, err, np.sum(s > 0), np.sum(S > 0)))
+        if err < delta:
+            return L, S
+
+    print("Convergence not reached in pcp!")
+    return L, S
